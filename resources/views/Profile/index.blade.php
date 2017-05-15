09a667aad2be958fe0eca279bbed2b46dd9fccd9
Francis <lagson6@yahoo.fr>
2017-05-14 20:02:25 -0700

    fixed

--- resources/views/Profile/index.blade.php
+++ resources/views/Profile/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-6">
+  <div class="col-md-4 col-md-offset-1">
     <img src="/uploads/avatars/{{ $user->profile_picture }}" style="width: 150px; height: 150px; border-radius: 50%; margin-right: 25px;"><br />
     <p>{{ $user->name }}<br />
     {{ $user->email }}<br />
@@ -15,7 +15,7 @@
     <a href="/Profile/upload">Upload Profile Picture</a>
     
   </div>
-  <div class="col-md-6">
+  <div class="col-md-4 col-md-offset-1">
     <h2>My Orders</h2>
     @foreach($orders as $order)
       <div class="panel panel-default">
